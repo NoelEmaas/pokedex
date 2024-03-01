7534338722a270e1e1a5461b5c38d1611c6de50b
NoelEmaas <nemaas@gbox.adnu.edu.ph>
2024-03-02 01:35:22 +0800

    feat: improved ui of pokemon cards

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,29 +1,31 @@
 import { useTypeIconsLoader } from '@/hooks/customPokemonHooks';
-import { transformString } from '@/lib/utils';
+import { transformString, getTypeColor } from '@/lib/utils';
 
 const PokemonCard = (props) => {
     const icons = useTypeIconsLoader(props.types);
-    
+    const color = getTypeColor(props.types[0].toLowerCase());
+
     return (
-        <div className="w-full bg-white border rounded-md">
-            <div className="p-2 border-b">
-                <div className='flex flex-row w-full gap-x-2'>
+        <div className="w-full bg-white border rounded-lg">
+            <div className="p-2">
+                <div className="flex flex-row w-full gap-x-2">
                     {props.types.map((type, index) => (
                         <div key={index}>
-                            {icons[type] && <img src={icons[type]} alt={type} width={85}/>}
+                            {icons[type] && <img src={icons[type]} alt={type} width={85} />}
                         </div>
                     ))}
                 </div>
-                <img src={props.image} alt={props.name} className='w-[500px] drop-shadow-lg'/>
+                <img src={props.image} alt={props.name} className="w-[500px] drop-shadow-lg" />
             </div>
-            <div className='flex flex-row justify-between px-4 py-3'>
-                <h1>{transformString(props.name)}</h1>
-                <div className=''>
-                    <h1>#{String(props.id).padStart(3, "0")}</h1>
+            <div className="flex flex-row items-center justify-between px-4 py-3 rounded-b-lg" style={{ backgroundColor: color }}>
+                <h1 className="font-bold text-black text-opacity-65">{transformString(props.name)}</h1>
+                <div className="flex items-center px-2 py-1 bg-black rounded-md bg-opacity-30">
+                    <h1 className="text-sm font-medium text-white">#{String(props.id).padStart(3, "0")}</h1>
                 </div>
             </div>
         </div>
-    );
+      );
+      
 }
 
 export default PokemonCard;
