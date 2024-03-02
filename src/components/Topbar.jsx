2fd7e05fd9d5ffd75bba00ee6923aa87c4959872
NoelEmaas <nemaas@gbox.adnu.edu.ph>
2024-03-02 20:31:59 +0800

    feat: added temporary topbar

---
+++ src/components/Topbar.jsx
@@ -0,0 +1,40 @@
+import { Input } from "@/components/ui/input"
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+  } from "@/components/ui/select"
+
+const Topbar = () => {
+    return (
+        <div className="w-full border-b topbar pt-[30px] pb-[20px] flex flex-row justify-between">
+            <div>
+                <h1 className='pb-4 text-6xl title'>Pokedex</h1>
+                <p>A database of Pokémon information at your fingertips.</p>
+            </div>
+            <div className="flex flex-row items-end">
+                <Input type="text" placeholder="Search pokemon" />
+                <Select>
+                    <SelectTrigger>
+                        <SelectValue placeholder="Sort by" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            <SelectLabel>Sort by</SelectLabel>
+                            <SelectItem value="apple">Name ↑</SelectItem>
+                            <SelectItem value="banana">Name ↓</SelectItem>
+                            <SelectItem value="blueberry">ID number ↑</SelectItem>
+                            <SelectItem value="grapes">ID number ↓</SelectItem>
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+            </div>
+        </div>
+    )
+}
+
+export default Topbar
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import PokemonList from '@/components/PokemonList';
+import Topbar from '@/components/Topbar';
 
 const Home = (props) => {
     return (
         <div className='h-screen home'>
-            <div className='container h-full bg-white border-x-4 border-[#701616]'>
+            <div className='container h-full bg-white border-x-4 xl:border-[#701616]'>
+                <Topbar />
                 <PokemonList />
             </div>
         </div>
