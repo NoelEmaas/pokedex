13c704fd79feec9a4b89cd7b3cb8c29cbcc1f85f
NoelEmaas <nemaas@gbox.adnu.edu.ph>
2024-03-02 12:34:31 +0800

    feat: added home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import PokemonList from '@/components/PokemonList';
+
+const Home = (props) => {
+    return (
+        <div className='container'>
+            <PokemonList />
+        </div>
+    )
+}
+
+export default Home
