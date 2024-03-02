9df5fb492382d9d1f2cdad8909bd966d4877dd39
NoelEmaas <nemaas@gbox.adnu.edu.ph>
2024-03-02 17:50:32 +0800

    feat: added a link in pokemon cards to redirect to details page

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,6 +1,7 @@
 import { useTypeIconsLoader } from '@/hooks/customPokemonHooks';
 import { transformString, getTypeColor } from '@/lib/utils';
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const PokemonCard = (props) => {
     const icons = useTypeIconsLoader(props.types);
@@ -19,28 +20,30 @@ const PokemonCard = (props) => {
     }, []); 
 
     return (
-        <div className={`w-full rounded-lg pokemon-card ${props.types[0].toLowerCase()}-card`}>
-            <div className="flex flex-row w-full px-2 pt-2 gap-x-2">
-                {props.types.map((type, index) => (
-                    <div key={index}>
-                    {windowWidth < 500 ? (
-                        icons[type] && <img src={icons[type]} alt={type} width={30} />
-                    ) : (
-                        icons[`${type}_full`] && <img src={icons[`${type}_full`]} alt={type} width={85} />
-                    )}
+        <Link to={`pokemon/${props.id}`}>
+            <div className={`w-full rounded-lg pokemon-card ${props.types[0].toLowerCase()}-card`}>
+                <div className="flex flex-row w-full px-2 pt-2 gap-x-2">
+                    {props.types.map((type, index) => (
+                        <div key={index}>
+                        {windowWidth < 500 ? (
+                            icons[type] && <img src={icons[type]} alt={type} width={30} />
+                        ) : (
+                            icons[`${type}_full`] && <img src={icons[`${type}_full`]} alt={type} width={85} />
+                        )}
+                        </div>
+                    ))}
+                </div>
+                <div className='px-2 md:min-h-[250px] min-h-[180px] flex items-center pokemon-img-container'>
+                    <img src={props.image} alt={props.name} className="pokemon-img" />
+                </div>
+                <div className="flex flex-row items-center justify-between px-4 py-3 rounded-b-lg" style={{ backgroundColor: color }}>
+                    <h1 className="font-bold text-white text-opacity-90 pokemon-name">{transformString(props.name)}</h1>
+                    <div className="flex items-center px-2 py-1 bg-black rounded-md bg-opacity-30">
+                        <h1 className="text-sm font-medium text-white">#{String(props.id).padStart(3, "0")}</h1>
                     </div>
-                ))}
-            </div>
-            <div className='px-2 md:min-h-[250px] min-h-[180px] flex items-center pokemon-img-container'>
-                <img src={props.image} alt={props.name} className="pokemon-img" />
-            </div>
-            <div className="flex flex-row items-center justify-between px-4 py-3 rounded-b-lg" style={{ backgroundColor: color }}>
-                <h1 className="font-bold text-white text-opacity-90 pokemon-name">{transformString(props.name)}</h1>
-                <div className="flex items-center px-2 py-1 bg-black rounded-md bg-opacity-30">
-                    <h1 className="text-sm font-medium text-white">#{String(props.id).padStart(3, "0")}</h1>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 }
 
